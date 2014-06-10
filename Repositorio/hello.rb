ffbc9c6ca461b2078cadd9a86524c16bd7ecc904
YenniCanul <yenni_canul@hotmail.com>
2014-06-09 23:04:10 -0600

    first commit

---
+++ Repositorio/hello.rb
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HolaMundo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!!");
+            Console.WriteLine("");
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
